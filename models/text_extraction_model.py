73ac8d6ca4c580ef9aea7b86141619c462cc0283
VedikaH <118132147+VedikaH@users.noreply.github.com>
2024-08-20 01:58:39 +0530

    Update text_extraction_model.py

--- models/text_extraction_model.py
+++ models/text_extraction_model.py
@@ -2,13 +2,6 @@ import pytesseract
 from PIL import Image
 import shutil
 
-def find_tesseract_binary() -> str:
-    return shutil.which("tesseract")
-
-# Set the tesseract binary path
-pytesseract.pytesseract.tesseract_cmd = find_tesseract_binary()
-st.write("Tesseract binary path:", pytesseract.pytesseract.tesseract_cmd)
-
 class TextExtractionModel:
     def __init__(self):
         # No initialization needed for pytesseract
